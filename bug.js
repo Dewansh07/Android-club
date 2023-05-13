9df8d0944d81a0873de0b1fd82e82fe532a4fee6
dewansh07 <dewanshwadhwa@gmail.com>
2023-05-13 13:52:12 +0530

    first commit

---
+++ bug.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    document.getElementById("starter").focus();
+};
+
+document.addEventListener("keydown", detectSpaceKey);
+
+function detectSpaceKey(event) {
+    if (event.keyCode == 32) {
+        showTime();
+    }
+}
+
+function showTime() {
+    var curtain = document.getElementById("curtain");
+    curtain.className = "open";
+
+    var scene = document.getElementById("scene");
+    scene.className = "expand";
+
+    var starter = document.getElementById("starter");
+    starter.className = "fade-out";
+
+    setTimeout(function () {
+        starter.style.display = "none";
+    }, 2000);
+}
